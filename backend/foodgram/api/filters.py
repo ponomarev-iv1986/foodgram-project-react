7a0676327b4b1e47071ffceeae707a404b26af52
Иван Пономарев <panama.piv@yandex.ru>
2023-07-17 17:20:58 +0200

    deploy

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,11 +1,13 @@
 from django_filters import FilterSet, filters
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 from rest_framework.filters import SearchFilter
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.CharFilter(
+    tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
     )
     is_favorited = filters.NumberFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.NumberFilter(
